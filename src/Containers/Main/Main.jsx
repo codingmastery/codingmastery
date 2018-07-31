6c83850e297e7beea4ca15d5d45e0262f71976af
Ignacio Garcia Villanueva <ivillanueva@deloitte.com.au>
2018-07-31 13:46:40 +1000

    Added hidden form. Removed extra form props from form-box

--- src/Containers/Main/Main.jsx
+++ src/Containers/Main/Main.jsx
@@ -57,7 +57,7 @@ class Main extends React.Component {
               <h1> Learn to code </h1>
               <p>It's easy. We provide all the steps!</p>
               <form name="subscribe_home" netlify netlify-honeypot="bot-field" hidden>
-                <input type="email" name="email" />
+                <input type="email" name="email" value={this.state.email} />
               </form>
               <form
                 className="form-box"
